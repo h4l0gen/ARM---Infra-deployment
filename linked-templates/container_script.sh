2fc852e6eef87f863afacccc692e0af3645bf92e
Kapil Sharma <ks3913688@gmail.com>
2025-07-31 16:17:51 +0530

    Create container_script.sh

---
+++ linked-templates/container_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting Elasticsearch and Kibana setup script..."
+echo "Waiting for services to be fully ready..."
+sleep 180  # Wait 3 minutes for both services to start
+
+# Get the Kibana URL from Container Apps
+KIBANA_FQDN=$(az containerapp show \
+  --name $KIBANA_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --query "properties.configuration.ingress.fqdn" -o tsv)
+
+ELASTICSEARCH_FQDN=$(az containerapp show \
+  --name $ELASTICSEARCH_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --query "properties.configuration.ingress.fqdn" -o tsv)
+
+KIBANA_URL="https://$KIBANA_FQDN"
+ELASTIC_URL="http://$ELASTICSEARCH_FQDN:9200"
+
+echo "Kibana URL: $KIBANA_URL"
+echo "Elasticsearch URL: $ELASTIC_URL"
